14903f05698b953b59ce4dc5d0a4641171a9bc4f
Vinicius Benedito <beneditoloura@gmail.com>
2021-03-11 06:21:05 -0300

    Trabalhando com eventos da classe EventEmitter

---
+++ mod 1 - Sincronia de funcoes JS/events/index.js
@@ -0,0 +1,33 @@
+const EventEmitter = require('events');
+
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuairo:click'
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+const stdin = process.openStdin()
+function main() {
+    return new Promise(function (resolve, reject) {
+        stdin.addListener('data', function (value) {
+            // console.log('voce digitou', value.toString().trim())
+            return resolve(value)
+        })
+    })
+}
+
+main().then(function (resultado) {
+    console.log('resultado', resultado)
+})
+
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+// meuEmissor.emit(nomeEvento, 'no ok')
+
+// let count = 0
+// setInterval(() => {
+//     meuEmissor.emit(nomeEvento, 'no ok' + (count ++))
+// }, 1000);
