912131ac6c3f31dc02111ab72ce31e77906719b8
Vinicius Benedito <beneditoloura@gmail.com>
2021-03-12 06:26:40 -0300

    Criando nosso Proprio Array.map

---
+++ mod2 - Manipulação de listas/lists/for-forin/2.map.js
@@ -0,0 +1,33 @@
+const service = require('./services')
+
+Array.prototype.meuMap = function (callback) {
+    const novoArrayMapeado = []
+    for (let indice = 0; indice <= this.length - 1; indice++) {
+        const resultado = callback(this[indice], indice);
+        novoArrayMapeado.push(resultado)
+    }
+
+    return novoArrayMapeado
+}
+
+async function main() {
+    try {
+        const result = await service.obterPessoas(`a`)
+        // const names = [];
+        // result.results.forEach(function (item) {
+        //     names.push(item.name)
+        // })
+        // const names = result.results.map(function (pessoa) {
+        //     return pessoa.name
+        // })
+        // const names = result.results.map((pessoa) => pessoa.name)
+        const names = result.results.meuMap(function (pessoa, indice) {
+            return `[${indice}]${pessoa.name}`
+        })
+        console.log('names', names)
+    } catch (error) {
+        console.error('error', error)
+    }
+}
+
+main()
