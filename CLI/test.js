50dade15466078da59188c5eef5a9a1a47ed969a
Vinicius Benedito <beneditoloura@gmail.com>
2021-03-17 07:33:23 -0300

    Manipulando arquivos -UPDATE

--- CLI/test.js
+++ CLI/test.js
@@ -2,9 +2,11 @@ const { deepEqual, ok } = require('assert')
 const database = require('./database')
 const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1 }
 
+const DEFAULT_ITEM_ATUALIZAR = { nome: 'Lanterna Verde', poder: 'Energia no Anel', id: 2 }
 describe('Suite de manipulação de Herois', () => {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
     })
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
@@ -23,5 +25,13 @@ describe('Suite de manipulação de Herois', () => {
         const expected = true
         const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
         deepEqual(resultado, expected)
+    })
+    it('deve atualizar um heroi pelo id', async() => {
+        const expected = { ...DEFAULT_ITEM_ATUALIZAR, nome: 'Batman', poder: 'Dinheiro' }
+        const novoDado = { nome: 'Batman', poder: 'Dinheiro' }
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected)
+
     })
 })
