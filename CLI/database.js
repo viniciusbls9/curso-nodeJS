af5ea57c02c730594d323a444440a42ed245beec
Vinicius Benedito <beneditoloura@gmail.com>
2021-03-17 07:01:11 -0300

    Manipulando arquivos - CREATE E DELETE

--- CLI/database.js
+++ CLI/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util')
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 class Database {
     constructor() {
@@ -11,13 +12,35 @@ class Database {
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
     }
-    escreverArquivo() {
-
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+    }
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now()
+        const heroidComId = { id, ...heroi }
+        const dadosFinais = [...dados, heroidComId]
+        const resultado = await this.escreverArquivo(dadosFinais)
+        return resultado
     }
     async listar(id) {
         const dados = await this.obterDadosArquivo()
         const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
-        return dadosFiltrados 
+        return dadosFiltrados
+    }
+    async remover(id) {
+        if (!id) {
+            return await this.escreverArquivo([])
+        }
+
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if (indice === -1) {
+            throw Error('O usuário informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
     }
 }
 
